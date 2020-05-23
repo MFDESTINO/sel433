9ca87c7b4560212e588f5277ed29ae945f453e3e
Daniel Couto <daniel.couto64@gmail.com>
2020-05-23 13:58:17 -0300

    Update l1ex05.asm

--- lista1/l1ex05.asm
+++ lista1/l1ex05.asm
@@ -1,20 +1,21 @@
-	ORG	0
-	MOV	R0, #50h
-	MOV	R1, #00h
-	MOV	DPTR, #8200h
-LOOP:	MOVX	A, @DPTR
-	MOV	@R0, A
-	INC	R0
-	INC	DPTR
-	INC	R1
-	CJNE	A, #00h, LOOP
-	MOV	R0, #50h
-	MOV	DPTR, #8300h
-LOOP2:	MOV	A, @R0
-	MOVX	@DPTR, A
-	INC	R0
-	INC	DPTR
-	CJNE	A, #00h, LOOP2
-	DEC	R1
-	MOV	40h, R1
-	END
+        ORG        0
+        MOV        R0, #50h        ;início da área de rascunho na RAM interna
+        MOV        R1, #00h        ;registrador para a contagem
+        MOV        DPTR, #8200h    ;endereço inicial para cópia
+LOOP:   MOVX       A, @DPTR
+        MOV        @R0, A          ;copia para a área de rascunho
+        INC        R0
+        INC        DPTR
+        INC        R1              ;incrementa a contagem
+        CJNE       A, #00h, LOOP   ;se o dado for zero, interrompe o loop
+        
+        MOV        R0, #50h
+        MOV        DPTR, #8300h    ;endereço de destino da cópia
+LOOP2:  MOV        A, @R0
+        MOVX       @DPTR, A
+        INC        R0
+        INC        DPTR
+        CJNE       A, #00h, LOOP2  ;se o dado for zero, interrompe o loop
+        DEC        R1              ;decrementa a contagem pois contou o dado 00h
+        MOV        40h, R1
+        END
