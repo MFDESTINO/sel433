0013a850b1fc70c228a1fb0623e68e9e9c13ec3d
Daniel Couto <daniel.couto64@gmail.com>
2020-05-23 13:56:56 -0300

    Update l1ex02.asm

--- lista1/l1ex02.asm
+++ lista1/l1ex02.asm
@@ -1,11 +1,12 @@
-	ORG	0
-	CLR	A
-	MOV	R0, #50h
-	MOV	DPTR, #2200h
-LOOP1:	INC	A
-	MOV	@R0, A
-	MOVX	@DPTR, A
-	INC	R0
-	INC	DPTR
-	CJNE	A, #15h, LOOP1
-	END
+        ORG	0
+        CLR	A
+        MOV	R0, #50h            ;endereço inicial de escrita na RAM interna
+        MOV	DPTR, #2200h        ;endereço inicial de escrita na RAM externa
+	    
+LOOP1:	INC	A                   ;incrementa A
+        MOV	@R0, A              ;copia A para a RAM interna
+        MOVX    @DPTR, A            ;copia A para a RAM externa
+        INC	R0                  ;incrementa o endereço contido em R0
+        INC	DPTR                ;incrementa o endereço contido em DPTR
+        CJNE    A, #15h, LOOP1      ;se A chegar a 15 quebra o loop
+        END
