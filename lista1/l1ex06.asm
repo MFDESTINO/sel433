68d44e5555141437b4083d918af431b85f04c3ae
Daniel Couto <daniel.couto64@gmail.com>
2020-05-23 13:59:02 -0300

    Update l1ex06.asm

--- lista1/l1ex06.asm
+++ lista1/l1ex06.asm
@@ -1,24 +1,26 @@
-ORG 0
-CLR C
-MOV R0, #10h
-MOV R1, #20h
-MOVX A, @R0
-MOV R2, A
-MOVX A, @R1
-ADD A, R2
-MOV 40h, A
-INC R0
-INC R1
-MOVX A, @R0
-MOV R2, A
-MOVX A, @R1
-ADDC A, R2
-MOV 41h, A
-INC R0
-INC R1
-MOVX A, @R0
-MOV R2, A
-MOVX A, @R1
-ADDC A, R2
-MOV 42h, A
+ORG     0
+CLR     C
+MOV     R0, #10h   ;endereço de teste
+MOV     R1, #20h   ;endereço de teste
+MOVX    A, @R0     ;carrega A com o dado para onde R0 aponta da RAM externa
+MOV     R2, A      ;deixa o dado em R2 provisoriamente
+MOVX    A, @R1     ;carrega A com o dado para onde R1 aponta da RAM externa
+ADD     A, R2      ;soma os dados de R0 e R1
+MOV     40h, A     ;copia o resultado para 40h
+
+INC     R0         ;faz o endereço de R0 ser R0+1
+INC     R1         ;faz o endereço de R1 ser R1+1
+MOVX    A, @R0
+MOV     R2, A
+MOVX    A, @R1
+ADDC    A, R2      ;a soma agora considera o resultado do carry da soma anterior
+MOV     41h, A
+
+INC     R0         ;faz o endereço de R0 ser R0+2
+INC     R1         ;faz o endereço de R1 ser R1+2
+MOVX    A, @R0  
+MOV     R2, A
+MOVX    A, @R1
+ADDC    A, R2
+MOV     42h, A
 END
