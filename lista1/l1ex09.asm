739e950692b349407044e6bc313ec45dc42534d4
Daniel Couto <daniel.couto64@gmail.com>
2020-05-25 18:19:56 -0300

    Update l1ex09.asm

--- lista1/l1ex09.asm
+++ lista1/l1ex09.asm
@@ -1,13 +1,12 @@
 	ORG	0
-	MOV	R0, #00h
+	MOV	R0, #08h
 	MOV	20h, #0Ah
-	MOV	A, 20h
-LOOP:	RLC	A
-	MOV	B, A
-	MOV	A, 2Fh
-	RRC	A
-	MOV	2Fh, A
-	MOV     A, B
-	INC	R0
-	CJNE	R0, #08h, LOOP
+	MOV	A, 20h   ;copia o byte desejado pro A
+LOOP:	RLC	A        ;rotaciona movendo o MSB para o carry
+	MOV	B, A     ;faz uma copia provisoria de A
+	MOV	A, 2Fh   ;copia o resultado pro A
+	RRC	A        ;insere o carry no MSB
+	MOV	2Fh, A   ;salva o resultado
+	MOV     A, B     ;volta A para onde parou
+	DJNZ	R0, LOOP ;faz o loop 8 vezes
 	END	0
